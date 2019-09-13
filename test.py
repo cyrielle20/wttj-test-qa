d615731482c1ed3891c8c6f7f1cf30a961948778
Cyrielle Mehl <49674407+cyrielle20@users.noreply.github.com>
2019-09-13 17:18:16 +0200

    Add files via upload

--- test.py
+++ test.py
@@ -2,40 +2,51 @@
 import csv
 import sys
 
-def list_category_id(file):
 
-    file = open(file, "r")
+def list_profession_id(profession_file):
+
+    file = open(profession_file, "r")
     reader=csv.DictReader(file)
-    cat_id = {}
+    profession_ids = {}
     for row in reader:
-        if (row['category_name'] not in cat_id.keys()):
-            cat_id[row['category_name']] = [row['id']]
+        if (row['category_name'] not in profession_ids.keys()):
+            profession_ids[row['category_name']] = [row['id']]
         else:
-            cat_id[row['category_name']].append(row['id'])
-    return cat_id
+            profession_ids[row['category_name']].append(row['id'])
+    file.close()
+    return profession_ids
 
 
-def count_job_contract_type(file, ids):
-    file_job = open(file, "r")
-    reader=csv.DictReader(file_job)
-    contract_type_count = {}
-    total = 0
+def count_jobs_by_profession_and_contract(profession_file, job_file):
+    profession_ids=list_profession_id(profession_file)
+    file = open(job_file, "r")
+    reader=csv.DictReader(file)
+    profession_id_contract_type_count = {}
     for row in reader:
-        if (row['profession_id'] in ids):
-            total = total + 1
-            if (row['contract_type'] not in contract_type_count):
-                contract_type_count[row['contract_type']] = 1
-            else:
-               contract_type_count[row['contract_type']] = contract_type_count[row['contract_type']] + 1
-    file_job.close()
-    return contract_type_count, total
+        for key, values in profession_ids.items():
+            if row['profession_id'] in values:
+                category_name = key
+        if category_name not in profession_id_contract_type_count:
+            profession_id_contract_type_count[category_name] = {}
 
-def list_contract_by_category(file1, file2):
-    cat_id=list_category_id(file1)
-    for key in cat_id.keys():
-        count, total = count_job_contract_type(file2,  cat_id[key])
-        import ipdb; ipdb.set_trace()
-        print(key, total, count)
+        if (row['contract_type']) not in profession_id_contract_type_count[category_name]:
+            profession_id_contract_type_count[category_name][row['contract_type']] = 1
+        else:
+            profession_id_contract_type_count[category_name][row['contract_type']] += 1
+
+    file.close()
+    return profession_id_contract_type_count
 
 
-list_contract_by_category(sys.argv[1], sys.argv[2])
+def print_jobs_by_profession_and_contract(profession_file, job_file):
+        map = count_jobs_by_profession_and_contract(profession_file, job_file)
+        for key, values in map.items():
+            total = 0
+            print(key)
+            for val in sorted(values):
+                print("    ", val, values[val])
+                total = total + values[val]
+            print(" "+"TOTAL", total)
+
+
+print_jobs_by_profession_and_contract(sys.argv[1], sys.argv[2])
